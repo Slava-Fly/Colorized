76c44770014b66c50541a7378198d67d9c49d4e8
@Slava-Fly <school.94@bk.ru>
2023-04-25 16:45:01 +0800

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by User on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
